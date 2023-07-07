9484b82bf915043294b18b88976078d52a503b3e
suusara <smataeva09@gmail.com>
2023-07-07 23:53:08 +0600

    add JS

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
+const loginForm = document.getElementById('login-form');
+const signupForm = document.getElementById('signup-form');
+const loginLink = document.getElementById('login-link');
+const signupLink = document.getElementById('signup-link');
 
+loginLink.addEventListener('click', (event) => {
+  event.preventDefault();
+  signupForm.style.display = 'none';
+  loginForm.style.display = 'block';
+
+  setTimeout(() => {
+    signupForm.style.opacity = 0;
+    loginForm.style.opacity = 1;
+  }, 10);
+});
+
+signupLink.addEventListener('click', (event) => {
+  event.preventDefault();
+  loginForm.style.display = 'none';
+  signupForm.style.display = 'block';
+
+  setTimeout(() => {
+    loginForm.style.opacity = 0;
+    signupForm.style.opacity = 1;
+  }, 10);
+});
